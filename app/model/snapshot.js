f87c274d880a724849d19c12c8afc1527bcde9b8
sylvainDNS <sylvain.denyse@gmail.com>
2018-02-02 14:44:51 +0100

    update model

--- app/model/snapshot.js
+++ app/model/snapshot.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const snapshotSchema = new Schema({
+const Snapshot = new Schema({
     dataURL: { type: String, required: true },
     date: { type: Date, required: false, default: Date.now },
     user: { type: String, required: true },
     title: { type: String, required: true }
 })
 
-module.exports = mongoose.model('Snapshot', snapshotSchema)
+module.exports = mongoose.model('Snapshot', Snapshot)
